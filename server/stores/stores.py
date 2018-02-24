a6cf91e69f737ae31e5374bd8bf0bb87eecdfcff
Nasy <sy_n@me.com>
2018-02-24 12:36:00 +0800

    Start stores

---
+++ server/stores/stores.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+r"""
+Life's pathetic, have fun ("▔□▔)/hi~♡ Nasy.
+
+Excited without bugs::
+
+    |             *         *
+    |                  .                .
+    |           .
+    |     *                      ,
+    |                   .
+    |
+    |                               *
+    |          |\___/|
+    |          )    -(             .              '
+    |         =\  -  /=
+    |           )===(       *
+    |          /   - \
+    |          |-    |
+    |         /   -   \     0.|.0
+    |  NASY___\__( (__/_____(\=/)__+1s____________
+    |  ______|____) )______|______|______|______|_
+    |  ___|______( (____|______|______|______|____
+    |  ______|____\_|______|______|______|______|_
+    |  ___|______|______|______|______|______|____
+    |  ______|______|______|______|______|______|_
+    |  ___|______|______|______|______|______|____
+
+* author: Nasy https://nasy.moe
+* date: Feb 24, 2018
+* email: echo bmFzeXh4QGdtYWlsLmNvbQo= | base64 -D
+* file: server/stores/stores.py
+* license: MIT
+
+There are more things in heaven and earth, Horatio, than are dreamt.
+ --  From "Hamlet"
+"""
+
+
+def main() -> None:
+    """Yooo, here is the main function."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
